9d5ead46f362726a1c1a22cb921337245e9a9eb9
bellacocho123 <isabelsky59@gmail.com>
2023-09-07 17:11:22 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program thats prints exactly a new line
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
