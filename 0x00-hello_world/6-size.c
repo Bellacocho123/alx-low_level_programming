e88ae55c413c94474172665d455600582966f60a
bellacocho123 <isabelsky59@gmail.com>
2023-09-07 17:44:27 -0700

    Intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the size of various types of the computer compiler
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s)", sizesof(char));
+	printf("size of int: %lu byte(s)", sizesof(int));
+	printf("size of h")
+}
