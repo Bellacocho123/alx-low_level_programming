2f322c022e2ae1363f48c27b990329afcd765cb4
bellacocho123 <isabelsky59@gmail.com>
2023-09-20 13:19:23 -0700

    strcmp

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * Main function for generating and printing combinations of four digits.
+ *
+ * This program generates all combinations of four digits (0-9) where the first
+ * pair is less than the second pair and prints them to the standard output.
+ *
+ * @return 0 on successful execution.
+ */
+
 int main(void)
 {
 	int n;
@@ -10,5 +19,5 @@ int main(void)
 	p = &n;
 	p[5] = 98;
 	printf("a[2] = %d\n", a[2]);
-	return 0;
+	return (0);
 }
