f98345ec7014a529c4b8adde353727bcdd877e81
bellacocho123 <isabelsky59@gmail.com>
2023-09-20 13:55:45 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Add one line to this code, so that the program prints
+ * You are not allowed to use the variable a in your new line of code
+ * You are not allowed to modify the variable p
+ * You can only write one statement
+ * Your code should be written at line 19, before the ;
+ * You are allowed to use the standard library
+ * Return: 0 on success
+ */
 
 int main(void)
 {
