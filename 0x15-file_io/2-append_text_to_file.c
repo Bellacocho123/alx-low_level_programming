e94d696c0ee0050f940e751bb2be7ce94fff5a6b
bellacocho123 <isabelsky59@gmail.com>
2023-10-30 16:59:35 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,9 +26,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
-		return (-1);
+		return (-1;
 
-	close(o)
+	close(o);
 
 	return (1);
 }
