9e2ff315eb904543c7c5e9e960d7807578cf2267
bellacocho123 <isabelsky59@gmail.com>
2024-04-11 14:43:43 -0700

    a function that searches for a value in a sorted array of integers using the Exponential search algorithm

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,21 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
+ * min - returns the min of two values
+ * @a: int a
+ * @b: int b
+ * Return: a || b
  */
-int main(void)
-{
-    int array[] = {
-        0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 54, 61, 62, 76, 99
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
 
-    printf("Found %d at index: %d\n\n", 62, exponential_search(array, size, 62));
-    printf("Found %d at index: %d\n\n", 3, exponential_search(array, size, 3));
-    printf("Found %d at index: %d\n", 999, exponential_search(array, size, 999));
-    return (EXIT_SUCCESS);
+size_t min(size_t a, size_t b)
+{
+	if (a > b)
+		return (b);
+	return (a);
+}
+
+/**
+ * binary_s - searches for a value using binary search
+ * @array: pointer to first element
+ * @size: size of the array
+ * @start: start postion
+ * @value: value to find
+ * Return: index if found or -1;
+ */
+
+int binary_s(int *array, int start, size_t size, int value)
+{
+
+	int l = start;
+	int r = size - 1;
+
+	while (l <= r)
+	{
+		int mid = l + (r - l) / 2;
+		int i = l;
+
+		printf("Searching in array: ");
+		for (; i < r; i++)
+			printf("%i, ", array[i]);
+		printf("%i\n", array[r]);
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			r = mid - 1;
+		else
+			l = mid + 1;
+	}
+	return (-1);
+}
+
+/**
+ * exponential_search - searches for a value using exponential search
+ * @array: pointer to first element
+ * @size: size of the array
+ * @value: value to find
+ * Return: index if found or -1;
+ */
+
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t i = 1;
+	size_t minimum;
+
+	if (!array)
+		return (-1);
+	while (i < size && array[i] <= value)
+	{
+		printf("Value checked array[%li] = [%d]\n", i, array[i]);
+		i *= 2;
+	}
+	minimum = min(i, size - 1);
+	printf("Value found between indexes [%li] and [%li]\n", i / 2, minimum);
+	return (binary_s(array, i / 2, min(i, size), value));
 }
